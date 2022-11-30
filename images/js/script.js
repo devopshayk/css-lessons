7e0a23d6ad575e916fef4f38c6de22c630169ca9
devopshayk <haykpoghosyan2009@gmail.com>
2022-11-30 22:37:51 +0400

    lesson 58

--- images/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $(".btn").click(function(){
-      var toggleText = $(this).data('toggle-text');
-      $(this).data('toggle-text', $(this).text())
-             .text(toggleText);
-    $(".description").toggleClass('show');
-    });
-  });
